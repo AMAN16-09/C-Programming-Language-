638aa4c50db6e2ece0e3bf23d1c21d517eb23fa4
Aman Bansal <bansalaman934@gmail.com>
2025-01-08 11:19:57 +0530

    Create Addition Matrix.c

---
+++ MATRIX/Addition Matrix.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#define MAX 10 
+void addMatrices(int a[MAX][MAX], int b[MAX][MAX], int result[MAX][MAX], int rows, int cols) 
+{
+    for (int i = 0; i < rows; i++) 
+    {
+        for (int j = 0; j < cols; j++) 
+        {
+            result[i][j] = a[i][j] + b[i][j];
+        }
+    }
+}
+
+int main() 
+{
+    int a[MAX][MAX], b[MAX][MAX], result[MAX][MAX];
+    int rows, cols;
+
+    printf("Enter the number of rows and columns: ");
+    scanf("%d %d", &rows, &cols);
+
+    printf("Enter elements of the first matrix:\n");
+    for (int i = 0; i < rows; i++) 
+    {
+        for (int j = 0; j < cols; j++) 
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    printf("Enter elements of the second matrix:\n");
+    for (int i = 0; i < rows; i++) 
+    {
+        for (int j = 0; j < cols; j++) 
+        {
+            scanf("%d", &b[i][j]);
+        }
+    }
+
+    addMatrices(a, b, result, rows, cols);
+
+    printf("Resultant matrix after addition:\n");
+    for (int i = 0; i < rows; i++) 
+    {
+        for (int j = 0; j < cols; j++) 
+        {
+            printf("%d ", result[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
