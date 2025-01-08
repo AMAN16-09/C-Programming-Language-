c76b51249cb56deef5ab555b8a07a9dd7ca27959
Aman Bansal <bansalaman934@gmail.com>
2025-01-08 11:42:53 +0530

    Create Do while loop ex.c

---
+++ Condition Statements/Do while loop ex.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() 
+{
+    int number = 10;
+
+    do 
+    {
+        printf("%d\n", number);
+        number++;
+    } 
+    while (number <= 20);
+
+    return 0;
+}
