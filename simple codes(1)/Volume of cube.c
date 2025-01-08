5fecfc412e5521774f3cd456d837f92b34038426
Aman Bansal <bansalaman934@gmail.com>
2025-01-08 11:14:14 +0530

    Create Volume of cube.c

---
+++ simple codes(1)/Volume of cube.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+int r,volume;
+printf("Enter the radius of the cube: ");
+scanf("%d",&r);
+volume=r*r*r;
+printf("Volume of the cube is: %d",volume);
+return 0;
+}
