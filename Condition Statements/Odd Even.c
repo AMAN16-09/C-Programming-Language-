f3430aae77ba93fb7412b545f07285c05f2ac595
Aman Bansal <bansalaman934@gmail.com>
2025-01-08 11:27:41 +0530

    Create Odd Even.c

---
+++ Condition Statements/Odd Even.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(void)
+{
+  int a;
+
+printf("\nEnter any number");
+scanf("%d",&a);
+if(a%2==0)
+{
+  printf("even number");
+}
+else
+{
+  printf("odd number");
+}
+return 0;
+}
